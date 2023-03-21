d23a7058eb4d34b1080223a9fa5e6ad2f65916bd
Warikel <warikel2312@gmail.com>
2023-03-21 06:18:36 -0700

    2-print_alphabet_x10.c git commit -m 2-print_alphabet_x10.c git commit -m 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ *Return: 10x a-z
+ */
+void print_alphabet_x10(void)
+{
+int l, ten;
+
+ten = 0;
+
+while (ten < 0)
+{
+for (l = 0; l < 26; l++)
+{
+_putchar('a' + l);
+}
+_putchar('\n');
+}
+}
