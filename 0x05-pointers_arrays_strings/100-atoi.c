b11f243c7ee1eec8b6104ccf09275cb1abc3c0a0
Warikel <warikel2312@gmail.com>
2023-03-27 07:47:59 -0700

    fixed bugs

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,9 +7,27 @@
  */
 int _atoi(char *s)
 {
-for (s = str; *s != '\0' && *s >= '0' && *s <= '9'; ++s)
-  {
-    *converted_value = *converted_value *10 + *s - '0';
- }
- return (true);
+short boolean;
+	int i, minus, result;
+
+	i = minus = result = boolean = 0;
+	minus = -1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			minus *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result -= (s[i] - '0');
+			boolean = 1;
+		}
+		else if (boolean == 1)
+			break;
+		i++;
+	}
+	result *= minus;
+	return (result);
 }
