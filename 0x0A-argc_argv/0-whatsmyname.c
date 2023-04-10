19bc3b858489ae0eccd975d9f101b120a862490c
Warikel <warikel2312@gmail.com>
2023-04-10 11:30:36 -0700

    task 0-2

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc >= 1)
+{
+  printf("%s/n", argv[0]);
+}
+return (0);
+}
