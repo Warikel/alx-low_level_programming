759c85e9c3d382e997bcc876dbf24b29e3d56473
Warikel <warikel2312@gmail.com>
2023-04-10 15:37:56 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Print the result
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Always zero
+ */
+int main(int argc,  char *argv[])
+{
+int i;
+unsigned int j, sum = 0;
+char *e;
+
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+e = argv[i];
+
+for (j = 0; j < strlen(e); j++)
+{
+if (e[j] < 48 || e[j] > 57)
+{
+printf("error\n");
+return (1);
+}
+}
+sum += atoi(e);
+e++;
+}
+printf("%d/n", sum);
+}
+else
+{
+printf("0\n");
+}
+return (0);
+}
